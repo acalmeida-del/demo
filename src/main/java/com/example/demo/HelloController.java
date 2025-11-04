01ec312f6abb836beb523205619afd28cba8e6ca
acalmeida <acalmeida@deloitte.com>
2025-11-04 14:11:00 +0530

    Initial code setup for hello world application

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/hi/{name}")
+    public String sayHi(@PathVariable String name) {
+        return "Hi " + name;
+    }
+}
